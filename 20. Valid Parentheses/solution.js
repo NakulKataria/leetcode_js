a2ac46260c28fa5a8eeaa652e1c41ac84173e907
NakulKataria <nakulkofficial@gmail.com>
2025-02-14 22:20:17 +0530

    20

---
+++ 20. Valid Parentheses/solution.js
@@ -0,0 +1,22 @@
+function isValid(s) {
+    const stack = [];
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+          
+            stack.push(char);
+        } else {
+   
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
